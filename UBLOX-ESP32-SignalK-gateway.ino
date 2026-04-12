9247cac851ba14e19af1c5cda9ff5a45ef1e9bac
mkvesala <matti.k.vesala@gmail.com>
2026-04-12 21:37:57 +0300

    Added .ino and espnow_protocol.h

---
+++ UBLOX-ESP32-SignalK-gateway.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+// #include "UBXApplication.h"
+
+// === M A I N  P R O G R A M ===
+//
+// - Owns UBXApplication instance
+// - Initiates serial
+// - Executes app.loop() in the main loop()
+
+// UBXApplication app;
+
+void setup() {
+
+    Serial.begin(115200);
+    delay(47);
+
+    // app.begin();
+
+}
+
+void loop() {
+
+    // app.loop();
+
+}
